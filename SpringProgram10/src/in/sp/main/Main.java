bd4d7c15dce753c303257ab4acaf833bf8f98293
coder221225 <majorvvs@gmail.com>
2025-12-28 16:17:29 +0530

    DI Autowiring xml configuration file with byType autowiring

---
+++ SpringProgram10/src/in/sp/main/Main.java
@@ -0,0 +1,18 @@
+package in.sp.main;
+
+import org.springframework.context.ApplicationContext;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import in.sp.beans.Student;
+
+public class Main {
+    public static void main(String[] args) {
+        String config_location="/in/sp/resources/applicationContext.xml";
+        //Spring container creation
+        ApplicationContext context =new ClassPathXmlApplicationContext(config_location);
+        Student std =(Student)context.getBean("stdId");
+        std.display();
+    }
+
+}
